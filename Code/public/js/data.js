f56e62bd6bc18aeb62c3d3c11ce2cb57296aac55
Ninkuk <ninadk03@gmail.com>
2020-10-07 13:47:40 -0700

    separated data from functions and made the filter buttons responsive

---
+++ Code/public/js/data.js
@@ -0,0 +1,130 @@
+const buttons = [{
+    "tag": "all",
+    "obj": "",
+    "color": "#00BFA5",
+    "icon": "fas fa-check"
+},
+{
+    "tag": "java",
+    "obj": "",
+    "color": "#B55400",
+    "icon": "fab fa-java",
+},
+{
+    "tag": "firebase",
+    "obj": "",
+    "color": "#FFA611",
+    "icon": "fas fa-server"
+},
+{
+    "tag": "android",
+    "obj": "",
+    "color": "#3DDC84",
+    "icon": "fab fa-android"
+},
+{
+    "tag": "python",
+    "obj": "",
+    "color": "#FFD43B",
+    "icon": "fab fa-python"
+},
+{
+    "tag": "html",
+    "obj": "",
+    "color": "#F16529",
+    "icon": "fab fa-html5"
+},
+{
+    "tag": "css",
+    "obj": "",
+    "color": "#29A9DF",
+    "icon": "fab fa-css3-alt"
+},
+{
+    "tag": "js",
+    "obj": "",
+    "color": "#F8D732",
+    "icon": "fab fa-js"
+},
+{
+    "tag": "kotlin",
+    "obj": "",
+    "color": "#A05FFC",
+    "icon": ""
+}
+];
+
+
+const projects = [{
+    "img": "images/WebApp/P2F.jpg",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "tags": ['android', 'java', 'kotlin', 'firebase'],
+    "url": "https://github.com/Ninkuk/FBLA_Mobile_App_Development"
+},
+{
+    "img": "images/WebApp/PK.jpg",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "tags": ['html', 'css'],
+    "url": "https://github.com/Ninkuk/Paolettis-Kitchen"
+},
+{
+    "img": "images/WebApp/stephire.jpg",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "tags": ['html', 'css'],
+    "url": ""
+},
+{
+    "img": "images/WebApp/collated.jpg",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "tags": ['html', 'css'],
+    "url": ""
+},
+{
+    "img": "images/WebApp/pvlsites.jpg",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "tags": ['html', 'css'],
+    "url": ""
+},
+{
+    "img": "images/WebApp/stephire.jpg",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "tags": ['html', 'css'],
+    "url": ""
+},
+{
+    "img": "images/WebApp/discordbot.jpg",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "tags": ['js'],
+    "url": "https://github.com/Ninkuk/one-punch-discord-bot"
+},
+{
+    "img": "images/WebApp/",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "tags": [],
+    "url": ""
+},
+{
+    "img": "images/WebApp/",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "tags": [],
+    "url": ""
+},
+{
+    "img": "images/WebApp/",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "tags": [],
+    "url": ""
+},
+{
+    "img": "images/WebApp/",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "tags": [],
+    "url": ""
+},
+{
+    "img": "images/WebApp/",
+    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "tags": [],
+    "url": ""
+}
+];
--- Code/public/js/filter.js
+++ Code/public/js/filter.js
@@ -1,66 +1,10 @@
 const projectsContainer = document.getElementById('projects-container');
 const filterBtnsContainer = document.getElementById('filter-btns');
 
-const buttons = [{
-        "tag": "all",
-        "obj": "",
-        "color": "#00BFA5",
-        "icon": "fas fa-check"
-    },
-    {
-        "tag": "java",
-        "obj": "",
-        "color": "#B55400",
-        "icon": "fab fa-java",
-    },
-    {
-        "tag": "firebase",
-        "obj": "",
-        "color": "#FFA611",
-        "icon": "fas fa-server"
-    },
-    {
-        "tag": "android",
-        "obj": "",
-        "color": "#3DDC84",
-        "icon": "fab fa-android"
-    },
-    {
-        "tag": "python",
-        "obj": "",
-        "color": "#FFD43B",
-        "icon": "fab fa-python"
-    },
-    {
-        "tag": "html",
-        "obj": "",
-        "color": "#F16529",
-        "icon": "fab fa-html5"
-    },
-    {
-        "tag": "css",
-        "obj": "",
-        "color": "#29A9DF",
-        "icon": "fab fa-css3-alt"
-    },
-    {
-        "tag": "js",
-        "obj": "",
-        "color": "#F8D732",
-        "icon": "fab fa-js"
-    },
-    {
-        "tag": "kotlin",
-        "obj": "",
-        "color": "#A05FFC",
-        "icon": ""
-    }
-];
-
 function generateButtons() {
     buttons.map(btn => {
         const button = document.createElement('button');
-        button.classList.add('btn', 'btn-outline', 'col-2', 'm-1');
+        button.setAttribute('class', 'btn btn-raised col-5 col-md-2 m-1')
 
         const icon = document.createElement('i');
         icon.setAttribute('class', (btn['icon']));
@@ -80,7 +24,7 @@ function generateButtons() {
 
         btn["obj"].addEventListener('click', () => {
             resetButtons();
-            btn["obj"].setAttribute('class', 'btn btn-raised col-2 m-1');
+            btn["obj"].setAttribute('class', 'btn btn-raised col-5 col-md-2 m-1');
             btn["obj"].style.backgroundColor = btn['color'];
             btn["obj"].style.color = "#FFFFFF";
     
@@ -98,7 +42,7 @@ function generateButtons() {
 
 function resetButtons() {
     buttons.map(btn => {
-        btn["obj"].setAttribute('class', 'btn btn-outline col-2 m-1');
+        btn["obj"].setAttribute('class', 'btn btn-outline col-5 col-md-2 m-1');
         btn["obj"].style.backgroundColor = 'transparent';
         btn["obj"].style.color = btn["color"];
 
@@ -107,45 +51,6 @@ function resetButtons() {
         }
     });
 }
-
-
-const projects = [{
-        "img": "images/WebApp/P2F.jpg",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "tags": ['android', 'java', 'kotlin', 'firebase'],
-        "url": "https://github.com/Ninkuk/FBLA_Mobile_App_Development"
-    },
-    {
-        "img": "images/WebApp/PK.jpg",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "tags": ['html', 'css'],
-        "url": "https://github.com/Ninkuk/Paolettis-Kitchen"
-    },
-    {
-        "img": "images/WebApp/stephire.jpg",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "tags": ['html', 'css'],
-        "url": ""
-    },
-    {
-        "img": "images/WebApp/collated.jpg",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "tags": ['html', 'css'],
-        "url": ""
-    },
-    {
-        "img": "images/WebApp/pvlsites.jpg",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "tags": ['html', 'css'],
-        "url": ""
-    },
-    {
-        "img": "images/WebApp/stephire.jpg",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "tags": ['html', 'css'],
-        "url": ""
-    }
-];
 
 function showCards(tag) {
     projectsContainer.innerHTML = "";
