28451f13885b95b9002cf4dab04180062f4f2652
Ninkuk <ninadk03@gmail.com>
2020-10-06 19:44:59 -0700

    Filter buttons are now added dynamically

--- Code/public/js/filter.js
+++ Code/public/js/filter.js
@@ -1,63 +1,112 @@
-const showAllBtn = document.getElementById('show-all-btn');
-const javaBtn = document.getElementById('java-btn');
-const firebaseBtn = document.getElementById('firebase-btn');
-const androidBtn = document.getElementById('android-btn');
-const pythonBtn = document.getElementById('python-btn');
-const htmlBtn = document.getElementById('html-btn');
-const cssBtn = document.getElementById('css-btn');
-const kotlinBtn = document.getElementById('kotlin-btn');
-const kotlinBtnIcon = document.getElementById('kotlin-btn-icon');
 const projectsContainer = document.getElementById('projects-container');
+const filterBtnsContainer = document.getElementById('filter-btns');
 
 const buttons = [{
         "tag": "all",
-        "obj": showAllBtn,
+        "obj": "",
         "color": "#00BFA5",
         "icon": "fas fa-check"
     },
     {
         "tag": "java",
-        "obj": javaBtn,
+        "obj": "",
         "color": "#B55400",
         "icon": "fab fa-java",
     },
     {
         "tag": "firebase",
-        "obj": firebaseBtn,
+        "obj": "",
         "color": "#FFA611",
         "icon": "fas fa-server"
     },
     {
         "tag": "android",
-        "obj": androidBtn,
+        "obj": "",
         "color": "#3DDC84",
         "icon": "fab fa-android"
     },
     {
         "tag": "python",
-        "obj": pythonBtn,
+        "obj": "",
         "color": "#FFD43B",
         "icon": "fab fa-python"
     },
     {
         "tag": "html",
-        "obj": htmlBtn,
+        "obj": "",
         "color": "#F16529",
         "icon": "fab fa-html5"
     },
     {
         "tag": "css",
-        "obj": cssBtn,
+        "obj": "",
         "color": "#29A9DF",
         "icon": "fab fa-css3-alt"
     },
     {
+        "tag": "js",
+        "obj": "",
+        "color": "#F8D732",
+        "icon": "fab fa-js"
+    },
+    {
         "tag": "kotlin",
-        "obj": kotlinBtn,
+        "obj": "",
         "color": "#A05FFC",
         "icon": ""
     }
 ];
+
+function generateButtons() {
+    buttons.map(btn => {
+        const button = document.createElement('button');
+        button.classList.add('btn', 'btn-outline', 'col-2', 'm-1');
+
+        const icon = document.createElement('i');
+        icon.setAttribute('class', (btn['icon']));
+
+        if (btn['tag'] == 'kotlin') {
+            const kotlinIcon = document.createElement('img');
+            kotlinIcon.src = 'https://img.icons8.com/ios/15/FFFFFF/kotlin.png'
+            button.appendChild(kotlinIcon);
+        }
+
+        // button.innerText = btn['tag'];
+        button.appendChild(icon);
+        button.innerHTML += " " + btn['tag'];
+        btn['obj'] = button;
+
+        filterBtnsContainer.appendChild(button);
+
+        btn["obj"].addEventListener('click', () => {
+            resetButtons();
+            btn["obj"].setAttribute('class', 'btn btn-raised col-2 m-1');
+            btn["obj"].style.backgroundColor = btn['color'];
+            btn["obj"].style.color = "#FFFFFF";
+    
+            if (btn["tag"] == "kotlin") {
+                btn['obj'].firstChild.src = 'https://img.icons8.com/ios/15/FFFFFF/kotlin.png';
+            }
+    
+            showCards(btn["tag"]);
+        });
+
+    });
+    
+    resetButtons();
+}
+
+function resetButtons() {
+    buttons.map(btn => {
+        btn["obj"].setAttribute('class', 'btn btn-outline col-2 m-1');
+        btn["obj"].style.backgroundColor = 'transparent';
+        btn["obj"].style.color = btn["color"];
+
+        if (btn['tag'] == 'kotlin') {
+            btn['obj'].firstChild.src = 'https://img.icons8.com/ios/15/A05FFC/kotlin.png';
+        }
+    });
+}
 
 
 const projects = [{
@@ -70,43 +119,36 @@ const projects = [{
         "img": "images/WebApp/PK.jpg",
         "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "tags": ['html', 'css'],
-        "url": "https://github.com/Ninkuk/FBLA_Mobile_App_Development"
+        "url": "https://github.com/Ninkuk/Paolettis-Kitchen"
+    },
+    {
+        "img": "images/WebApp/stephire.jpg",
+        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "tags": ['html', 'css'],
+        "url": ""
+    },
+    {
+        "img": "images/WebApp/collated.jpg",
+        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "tags": ['html', 'css'],
+        "url": ""
+    },
+    {
+        "img": "images/WebApp/pvlsites.jpg",
+        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "tags": ['html', 'css'],
+        "url": ""
+    },
+    {
+        "img": "images/WebApp/stephire.jpg",
+        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "tags": ['html', 'css'],
+        "url": ""
     }
 ];
 
-buttons.map(btn => {
-    btn["obj"].addEventListener('click', () => {
-        resetButtons();
-        btn["obj"].setAttribute('class', 'btn btn-raised col-2 m-1');
-        btn["obj"].style.backgroundColor = btn['color'];
-        btn["obj"].style.color = "#FFFFFF";
-
-        if (btn["tag"] == "kotlin") {
-            kotlinBtnIcon.setAttribute('src', 'https://img.icons8.com/ios/15/FFFFFF/kotlin.png');
-        }
-
-        showCards(btn["tag"]);
-    })
-});
-
-
-function resetButtons() {
-    buttons.map(btn => {
-        btn["obj"].setAttribute('class', 'btn btn-outline col-2 m-1');
-        btn["obj"].style.backgroundColor = 'transparent';
-        btn["obj"].style.color = btn["color"];
-    });
-
-    kotlinBtnIcon.setAttribute('src', 'https://img.icons8.com/ios/15/A05FFC/kotlin.png');
-}
-
 function showCards(tag) {
     projectsContainer.innerHTML = "";
-
-    // if (tag === "all") {
-    //     showAllCards();
-    //     return;
-    // }
 
     projects.map(project => {
         if (project['tags'].includes(tag) || tag == 'all') {
