1718d65bf45a663cba01da9a04541cb07fb49c45
Ninkuk <ninadk03@gmail.com>
2020-12-20 16:37:45 -0700

    added dsc image

--- Code/public/js/data.js
+++ Code/public/js/data.js
@@ -68,15 +68,15 @@ const projects = [{
         "url": "https://github.com/Ninkuk/Paolettis-Kitchen"
     },
     {
-        "img": "images/WebApp/stephire.jpg",
-        "body": "",
-        "tags": ['html/css', 'js'],
-        "url": ""
-    },
-    {
         "img": "images/WebApp/pvlsites.jpg",
         "body": "As a class leader and mentor, I created the class site for Pinnacle High School's award-winning Graphic Design program.\n\nThis website is used by the course instructor along with senior students to promote the class to freshman and other prospective students during the club fair.\n\nThe site also showcases the projects and accomplishments of students that participate in CTE competitions on State and National stages.",
         "tags": ['html/css'],
+        "url": ""
+    },
+    {
+        "img": "images/WebApp/asu.jpg",
+        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "tags": ['python'],
         "url": ""
     },
     {
@@ -86,20 +86,8 @@ const projects = [{
         "url": "https://github.com/Ninkuk/one-punch-discord-bot"
     },
     {
-        "img": "images/WebApp/chaibaaz.jpg",
-        "body": "Being in an Indian family where every member has a different style of making Chai Tea, I always forget the recipes and measurements of particular styles.\n\nSo I made an app where you can store the measurement of ingredients and take specific notes to refer back to.",
-        "tags": ['android', 'kotlin'],
-        "url": "https://github.com/Ninkuk/ChaiBaaz"
-    },
-    {
         "img": "images/WebApp/wallpaper.jpg",
         "body": "Get a new wallpapers every hour from royalty-free sources like Pexels and Unsplash!\n\nThe script picks a random keyword from a list of hand picked search terms that will liven up your desktop screen without spending hours hunting down the perfect wallpaper.",
-        "tags": ['python'],
-        "url": ""
-    },
-    {
-        "img": "images/WebApp/asu.jpg",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "tags": ['python'],
         "url": ""
     },
@@ -110,10 +98,28 @@ const projects = [{
         "url": "https://github.com/Ninkuk/Instagram-Caption-Scraper"
     },
     {
+        "img": "images/WebApp/asudsc.jpg",
+        "body": "",
+        "tags": ['react', 'html/css', 'js'],
+        "url": ""
+    },
+    {
+        "img": "images/WebApp/stephire.jpg",
+        "body": "",
+        "tags": ['html/css', 'js'],
+        "url": ""
+    },
+    {
         "img": "images/WebApp/collated.jpg",
         "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "tags": ['html/css'],
         "url": ""
+    },
+    {
+        "img": "images/WebApp/chaibaaz.jpg",
+        "body": "Being in an Indian family where every member has a different style of making Chai Tea, I always forget the recipes and measurements of particular styles.\n\nSo I made an app where you can store the measurement of ingredients and take specific notes to refer back to.",
+        "tags": ['android', 'kotlin'],
+        "url": "https://github.com/Ninkuk/ChaiBaaz"
     },
 ];
 
